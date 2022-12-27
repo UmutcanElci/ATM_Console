c6c9d33fc14dffb251bdd1aab60d49ab71f63eb5
Umutcan Elçi <umutcanelci@yaani.com>
2022-12-27 15:09:47 +0300

    ATMFUNC

--- src/ATM/AtmFucntions.java
+++ src/ATM/AtmFucntions.java
@@ -1,5 +1,9 @@
 package ATM;
 
 public class AtmFucntions extends Currency{
-    
+    public void currencyConvert(String currency1,String currency2){
+       int a =  searchCurrency(currency1);
+       int b = searchCurrency(currency2);
+        System.out.println(a+b);
+    }
 }
