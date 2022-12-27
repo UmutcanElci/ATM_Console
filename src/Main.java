8f037e839823f4daad9a3d14968415349fa8561c
Umutcan Elçi <umutcanelci@yaani.com>
2022-12-27 15:09:57 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import ATM.AtmFucntions;
 import ATM.Currency;
 
 public class Main {
     public static void main(String[] args) {
-
+        AtmFucntions fucntions = new AtmFucntions();
+        
     }
 }
